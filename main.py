dd571a339dcd585581ce0b50420a2d2786735a0d
rspoonmore <rspoonmore@gmail.com>
2021-07-28 15:00:08 -0500

    seperate DF creation and excel generation into BowlExcelCreator.py

--- main.py
+++ main.py
@@ -1,48 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+# import GridSpec as gs
+import seaborn as sb
+from BowlExcelCreator import createBowlExcel
 
-rawResults = pd.read_excel('Results.xlsx')
-rawConferences = pd.read_excel('conferences.xlsx')
-results = rawResults.copy()
-conferences = rawConferences.copy().groupby('Year')['Conference'].value_counts().to_frame().rename(columns={'Conference': 'Teams'})
-divisions = rawConferences.copy().groupby(['Year', 'Conference'])['Division'].value_counts().to_frame().rename(columns={'Division': 'Teams'})
+excelFileName = 'College Football Bowl Game Analysis.xlsx'
 
-print('Results index is unique: %s' % results.index.is_unique)
-print('Conference index is unique: %s' % rawConferences.index.is_unique)
+[results, conferences, divisions] = createBowlExcel(excelFileName)
 
-results[['Team Conference', 'Team Division', 'Team Power Five']] = rawResults.merge(rawConferences, how='left', left_on=['School', 'Year'], right_on=['School', 'Year']).loc[:,['Conference', 'Division', 'Power Five']]
-results[['Opponent Conference', 'Opponent Division', 'Opponent Power Five']] = rawResults.merge(rawConferences, how='left', left_on=['Opponent', 'Year'], right_on=['School', 'Year']).loc[:,['Conference', 'Division', 'Power Five']]
-
-print('All games have team conference match: %s' % (results.count()['School'] == results.count()['Team Conference']))
-print('All games have opponent conference match: %s' % (results.count()['School'] == results.count()['Opponent Conference']))
-
-# Create DF of conferences and devisions with describe() 
-conferenceDescribe = results.groupby(['Year', 'Team Power Five', 'Team Conference']).describe().drop([('Team Points', 'count'), ('Opponent Points', 'count'), ('Team Pass', 'count'), ('Opponent Pass', 'count'), ('Team Rush', 'count'), ('Opponent Rush', 'count'), ('Opponent Yards', 'count'), ('Team Yards', 'count')], axis = 1)
-divisionDescribe = results.groupby(['Year', 'Team Power Five', 'Team Conference', 'Team Division']).describe().drop([('Team Points', 'count'), ('Opponent Points', 'count'), ('Team Pass', 'count'), ('Opponent Pass', 'count'), ('Team Rush', 'count'), ('Opponent Rush', 'count'), ('Opponent Yards', 'count'), ('Team Yards', 'count')], axis = 1)
-conferenceDescribe.index = conferenceDescribe.index.rename(['Year', 'Power Five', 'Conference'])
-divisionDescribe.index = divisionDescribe.index.rename(['Year', 'Power Five', 'Conference', 'Division'])
-conferenceDescribe.columns = [' '.join(col) for col in conferenceDescribe.columns.values]
-divisionDescribe.columns = [' '.join(col) for col in divisionDescribe.columns.values]
-conferenceDescribe.reset_index('Power Five', inplace=True)
-divisionDescribe.reset_index('Power Five', inplace=True)
-
-# Add win boolean
-results.insert(7, 'Win', results.apply(lambda x: x['Team Points'] > x['Opponent Points'], axis=1))
-
-# Create DF of conference and dvision placement and Win percentage and add to describes
-conferenceResults = results.groupby(['Year', 'Team Conference']).agg({'Bowl Game': 'count', 'Win': 'sum'}).rename(columns={'Bowl Game': 'Bowl Teams', 'Win':'Wins'})
-conferenceResults.index = conferenceResults.index.rename(('Year', 'Conference'))
-conferences[['Bowl Teams', 'Wins']] = conferences.merge(conferenceResults, how='inner', left_index=True, right_index=True).loc[:, ['Bowl Teams', 'Wins']]
-conferences['Placement %'] = conferences['Bowl Teams'] / conferences['Teams']
-conferences['Win %'] = conferences['Wins'] / conferences['Bowl Teams']
-conferences = conferences.merge(conferenceDescribe, how='left', left_index=True, right_index=True)
-divisionResults = results.groupby(['Year', 'Team Conference', 'Team Division']).agg({'Bowl Game': 'count', 'Win': 'sum'}).rename(columns={'Bowl Game': 'Bowl Teams', 'Win':'Wins'})
-divisionResults.index = divisionResults.index.rename(['Year', 'Conference', 'Division'])
-divisions[['Bowl Teams', 'Wins']] = divisions.merge(divisionResults, how='inner', left_index=True, right_index=True).loc[:, ['Bowl Teams', 'Wins']]
-divisions['Placement %'] = divisions['Bowl Teams'] / divisions['Teams']
-divisions['Win %'] = divisions['Wins'] / divisions['Bowl Teams']
-divisions = divisions.merge(divisionDescribe, how='left', left_index=True, right_index=True)
-
-
-print(divisions[divisions.index.get_level_values('Year') == 2020].head(5))
-print(divisions[divisions['Bowl Teams'] == 0])
-#print(conferenceDescribe.index)
+fig = plt.figure()
+sb.boxplot(data=results[results['Team Power Five'] == 'Power Five'], x='Year', y='Team Points', hue='Team Conference')
+plt.show()
+#print(resultsDescribe)
