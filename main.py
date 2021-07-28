d8528eb81ea5aa9bbfdf1ec2697c93a72cd68482
rspoonmore <rspoonmore@gmail.com>
2021-07-28 12:44:51 -0500

    create descriptive statistics on conferences and divisions

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import pandas as pd
 rawResults = pd.read_excel('Results.xlsx')
 rawConferences = pd.read_excel('conferences.xlsx')
 results = rawResults.copy()
-conferences = rawConferences.copy().groupby('Conference')['Year'].value_counts().to_frame().rename(columns={'Year': 'Teams'})
+conferences = rawConferences.copy().groupby('Year')['Conference'].value_counts().to_frame().rename(columns={'Conference': 'Teams'})
+divisions = rawConferences.copy().groupby(['Year', 'Conference'])['Division'].value_counts().to_frame().rename(columns={'Division': 'Teams'})
 
 print('Results index is unique: %s' % results.index.is_unique)
 print('Conference index is unique: %s' % rawConferences.index.is_unique)
@@ -14,17 +15,34 @@ results[['Opponent Conference', 'Opponent Division', 'Opponent Power Five']] = r
 print('All games have team conference match: %s' % (results.count()['School'] == results.count()['Team Conference']))
 print('All games have opponent conference match: %s' % (results.count()['School'] == results.count()['Opponent Conference']))
 
-conferenceDescribe = results.groupby(['Year', 'Team Power Five', 'Team Conference']).describe().drop([('Opponent Points', 'count'), ('Team Pass', 'count'), ('Opponent Pass', 'count'), ('Team Rush', 'count'), ('Opponent Rush', 'count'), ('Opponent Yards', 'count'), ('Team Yards', 'count')], axis = 1)
-DivisionDescribe = results.groupby(['Year', 'Team Power Five', 'Team Conference', 'Team Division']).describe().drop([('Opponent Points', 'count'), ('Team Pass', 'count'), ('Opponent Pass', 'count'), ('Team Rush', 'count'), ('Opponent Rush', 'count'), ('Opponent Yards', 'count'), ('Team Yards', 'count')], axis = 1)
+# Create DF of conferences and devisions with describe() 
+conferenceDescribe = results.groupby(['Year', 'Team Power Five', 'Team Conference']).describe().drop([('Team Points', 'count'), ('Opponent Points', 'count'), ('Team Pass', 'count'), ('Opponent Pass', 'count'), ('Team Rush', 'count'), ('Opponent Rush', 'count'), ('Opponent Yards', 'count'), ('Team Yards', 'count')], axis = 1)
+divisionDescribe = results.groupby(['Year', 'Team Power Five', 'Team Conference', 'Team Division']).describe().drop([('Team Points', 'count'), ('Opponent Points', 'count'), ('Team Pass', 'count'), ('Opponent Pass', 'count'), ('Team Rush', 'count'), ('Opponent Rush', 'count'), ('Opponent Yards', 'count'), ('Team Yards', 'count')], axis = 1)
+conferenceDescribe.index = conferenceDescribe.index.rename(['Year', 'Power Five', 'Conference'])
+divisionDescribe.index = divisionDescribe.index.rename(['Year', 'Power Five', 'Conference', 'Division'])
+conferenceDescribe.columns = [' '.join(col) for col in conferenceDescribe.columns.values]
+divisionDescribe.columns = [' '.join(col) for col in divisionDescribe.columns.values]
+conferenceDescribe.reset_index('Power Five', inplace=True)
+divisionDescribe.reset_index('Power Five', inplace=True)
 
+# Add win boolean
 results.insert(7, 'Win', results.apply(lambda x: x['Team Points'] > x['Opponent Points'], axis=1))
 
-conferenceResults = results.groupby(['Team Conference', 'Year']).agg({'Bowl Game': 'count', 'Win': 'sum'}).rename(columns={'Bowl Game': 'Bowl Teams', 'Win':'Wins'})
-conferenceResults.index = conferenceResults.index.rename(('Conference', 'Year'))
-
+# Create DF of conference and dvision placement and Win percentage and add to describes
+conferenceResults = results.groupby(['Year', 'Team Conference']).agg({'Bowl Game': 'count', 'Win': 'sum'}).rename(columns={'Bowl Game': 'Bowl Teams', 'Win':'Wins'})
+conferenceResults.index = conferenceResults.index.rename(('Year', 'Conference'))
 conferences[['Bowl Teams', 'Wins']] = conferences.merge(conferenceResults, how='inner', left_index=True, right_index=True).loc[:, ['Bowl Teams', 'Wins']]
-conferences['Placeemnt %'] = conferences['Bowl Teams'] / conferences['Teams']
+conferences['Placement %'] = conferences['Bowl Teams'] / conferences['Teams']
 conferences['Win %'] = conferences['Wins'] / conferences['Bowl Teams']
-print(conferences.head(1))
+conferences = conferences.merge(conferenceDescribe, how='left', left_index=True, right_index=True)
+divisionResults = results.groupby(['Year', 'Team Conference', 'Team Division']).agg({'Bowl Game': 'count', 'Win': 'sum'}).rename(columns={'Bowl Game': 'Bowl Teams', 'Win':'Wins'})
+divisionResults.index = divisionResults.index.rename(['Year', 'Conference', 'Division'])
+divisions[['Bowl Teams', 'Wins']] = divisions.merge(divisionResults, how='inner', left_index=True, right_index=True).loc[:, ['Bowl Teams', 'Wins']]
+divisions['Placement %'] = divisions['Bowl Teams'] / divisions['Teams']
+divisions['Win %'] = divisions['Wins'] / divisions['Bowl Teams']
+divisions = divisions.merge(divisionDescribe, how='left', left_index=True, right_index=True)
 
-#print(conferenceDescribe.head(5))
+
+print(divisions[divisions.index.get_level_values('Year') == 2020].head(5))
+print(divisions[divisions['Bowl Teams'] == 0])
+#print(conferenceDescribe.index)
